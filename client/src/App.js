a4f3c7b06ccdda40effa93f7e9ddd205bce70e11
Andy Bettisworth <andy@accreu.com>
2019-12-02 16:11:21 -0600

    Assert bold text

--- client/src/App.js
+++ client/src/App.js
@@ -34,15 +34,37 @@ function App() {
             <List
               id="articles"
               type="ordered"
-              style={{ listStyleType: 'none', padding: "0px 10px", margin: "0" }}
+              style={{
+                listStyleType: 'none',
+                padding: '0px 10px',
+                margin: '0'
+              }}
             >
               <ListItem className="article">
-                <Link href="#">Article #1</Link>
-                <hr/>
+                <Link href="#" style={{ color: 'rgb(0,0,0)', fontWeight: '700' }}>
+                  AWS announces DeepComposer, a machine-learning keyboard for
+                  developers
+                </Link>
+                <hr />
               </ListItem>
               <ListItem className="article">
-                <Link href="#">Article #2</Link>
-                <hr/>
+                <Link href="#" style={{ color: 'rgb(0,0,0)', fontWeight: '700' }}>
+                  Oil and gas slow to adopt artificial intelligence
+                </Link>
+                <hr />
+              </ListItem>
+              <ListItem className="article">
+                <Link href="#" style={{ color: 'rgb(0,0,0)', fontWeight: '700' }}>
+                  This is how Facebook's AI looks for bad stuff
+                </Link>
+                <hr />
+              </ListItem>
+              <ListItem className="article">
+                <Link href="#" style={{ color: 'rgb(0,0,0)', fontWeight: '700' }}>
+                  Data Scientists: Machine Learning Skills are Key to Future
+                  Jobs
+                </Link>
+                <hr />
               </ListItem>
             </List>
           </main>
