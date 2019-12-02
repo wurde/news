6ff4e9b310b6ff49eaba2812eb042195588d1cc5
Andy Bettisworth <andy@accreu.com>
2019-12-02 16:51:34 -0600

    Assert heading aligned to the right

--- client/cypress/integration/readability.spec.js
+++ client/cypress/integration/readability.spec.js
@@ -12,5 +12,9 @@ describe("Readability", () => {
     cy.get('main').should('has.css', 'background-color', 'rgb(255, 255, 255)');
     cy.get('a').should('has.css', 'color', 'rgb(0, 0, 0)')
     .and('has.css', 'font-weight', '700');
-  })
-})
+  });
+
+  it('De-emphasize heading', () => {
+    cy.get('h1').should('has.css', 'text-align', 'right');
+  });
+});
--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import {
   List,
   ListItem,
   Link,
+  Small,
 } from '@wurde/components';
 
 /**
@@ -24,8 +25,8 @@ function App() {
       <Row>
         <Column>
           <Header>
-            <Heading type="h1" style={{ fontSize: '1.8rem' }}>
-              News
+            <Heading type="h1" style={{ fontSize: '1rem', textAlign: 'right' }}>
+              News <Small>v0.1.0</Small>
             </Heading>
           </Header>
 
@@ -41,26 +42,38 @@ function App() {
               }}
             >
               <ListItem className="article">
-                <Link href="#" style={{ color: 'rgb(0,0,0)', fontWeight: '700' }}>
+                <Link
+                  href="#"
+                  style={{ color: 'rgb(0,0,0)', fontWeight: '700' }}
+                >
                   AWS announces DeepComposer, a machine-learning keyboard for
                   developers
                 </Link>
                 <hr />
               </ListItem>
               <ListItem className="article">
-                <Link href="#" style={{ color: 'rgb(0,0,0)', fontWeight: '700' }}>
+                <Link
+                  href="#"
+                  style={{ color: 'rgb(0,0,0)', fontWeight: '700' }}
+                >
                   Oil and gas slow to adopt artificial intelligence
                 </Link>
                 <hr />
               </ListItem>
               <ListItem className="article">
-                <Link href="#" style={{ color: 'rgb(0,0,0)', fontWeight: '700' }}>
+                <Link
+                  href="#"
+                  style={{ color: 'rgb(0,0,0)', fontWeight: '700' }}
+                >
                   This is how Facebook's AI looks for bad stuff
                 </Link>
                 <hr />
               </ListItem>
               <ListItem className="article">
-                <Link href="#" style={{ color: 'rgb(0,0,0)', fontWeight: '700' }}>
+                <Link
+                  href="#"
+                  style={{ color: 'rgb(0,0,0)', fontWeight: '700' }}
+                >
                   Data Scientists: Machine Learning Skills are Key to Future
                   Jobs
                 </Link>
