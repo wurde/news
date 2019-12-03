549c322f2e1844bd29dcf449ca60463532245465
Andy Bettisworth <andy@accreu.com>
2019-12-02 19:39:34 -0600

    Update cypress test scope

--- client/cypress/integration/readability.spec.js
+++ client/cypress/integration/readability.spec.js
@@ -10,7 +10,7 @@ describe("Readability", () => {
   it('Has accessible color contrast', () => {
     cy.get('body').should('has.css', 'background-color', 'rgb(221, 221, 221)');
     cy.get('main').should('has.css', 'background-color', 'rgb(255, 255, 255)');
-    cy.get('a').should('has.css', 'color', 'rgb(0, 0, 0)')
+    cy.get('#articles a').should('has.css', 'color', 'rgb(0, 0, 0)')
     .and('has.css', 'font-weight', '700');
   });
 
