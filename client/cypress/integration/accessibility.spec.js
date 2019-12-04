fcf024df4cafc7aad2e6ffeb7aca714033c7aebf
Andy Bettisworth <andy@accreu.com>
2019-12-04 02:07:02 -0600

    Explicitly set background to white

--- client/cypress/integration/accessibility.spec.js
+++ client/cypress/integration/accessibility.spec.js
@@ -2,13 +2,13 @@
  * Define assertions
  */
 
-describe("Readability", () => {
+describe("Accessibility", () => {
   beforeEach(() => {
     cy.visit('/');
   });
 
   it('Has accessible color contrast', () => {
-    cy.get('body').should('has.css', 'background-color', 'rgb(221, 221, 221)');
+    cy.get('body').should('has.css', 'background-color', 'rgb(255, 255, 255)');
     cy.get('main').should('has.css', 'background-color', 'rgb(255, 255, 255)');
     cy.get('#articles a').should('has.css', 'color', 'rgb(0, 0, 0)')
     .and('has.css', 'font-weight', '700');
