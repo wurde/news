d5ead404ad3631811fe6746f8f92128c4508bc72
Andy Bettisworth <andy@accreu.com>
2019-12-04 01:50:39 -0600

    Add useEffect for loading articles

--- client/src/components/ArticleList.js
+++ client/src/components/ArticleList.js
@@ -2,8 +2,8 @@
  * Dependencies
  */
 
-import React, { useState } from 'react';
-import { List } from '@wurde/components';
+import React, { useState, useEffect } from 'react';
+import { List, Loader } from '@wurde/components';
 import Article from './Article';
 
 /**
@@ -11,33 +11,42 @@ import Article from './Article';
  */
 
 function ArticleList() {
-  const [articles, setArticles] = useState([
-    {
-      title:
-        'AWS announces DeepComposer, a machine-learning keyboard for developers',
-      link:
-        'https://techcrunch.com/2019/12/02/aws-announces-deepcomposer-a-machine-learning-keyboard-for-developers/',
-      createdAt: new Date()
-    },
-    {
-      title: 'Oil and gas slow to adopt artificial intelligence',
-      link:
-        'https://www.houstonchronicle.com/business/columnists/tomlinson/article/Oil-and-gas-slow-to-adopt-artificial-intelligence-14861229.php',
-      createdAt: new Date()
-    },
-    {
-      title: "This is how Facebook's AI looks for bad stuff",
-      link:
-        'https://www.technologyreview.com/f/614774/this-is-how-facebooks-ai-looks-for-bad-stuff/',
-      createdAt: new Date()
-    },
-    {
-      title: 'Data Scientists: Machine Learning Skills are Key to Future Jobs',
-      link:
-        'https://insights.dice.com/2019/11/29/data-scientists-machine-learning-skills-key-future-jobs/',
-      createdAt: new Date()
-    }
-  ]);
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setArticles([
+        {
+          title:
+            'AWS announces DeepComposer, a machine-learning keyboard for developers',
+          link:
+            'https://techcrunch.com/2019/12/02/aws-announces-deepcomposer-a-machine-learning-keyboard-for-developers/',
+          createdAt: new Date()
+        },
+        {
+          title: 'Oil and gas slow to adopt artificial intelligence',
+          link:
+            'https://www.houstonchronicle.com/business/columnists/tomlinson/article/Oil-and-gas-slow-to-adopt-artificial-intelligence-14861229.php',
+          createdAt: new Date()
+        },
+        {
+          title: "This is how Facebook's AI looks for bad stuff",
+          link:
+            'https://www.technologyreview.com/f/614774/this-is-how-facebooks-ai-looks-for-bad-stuff/',
+          createdAt: new Date()
+        },
+        {
+          title:
+            'Data Scientists: Machine Learning Skills are Key to Future Jobs',
+          link:
+            'https://insights.dice.com/2019/11/29/data-scientists-machine-learning-skills-key-future-jobs/',
+          createdAt: new Date()
+        }
+      ]);
+    }, 1200)
+  }, [])
+
+  if (articles.length === 0) return <div>Loading</div>;
 
   return (
     <List
