d41c69bb09ce73e8d033c7b4b499219a45670f08
Andy Bettisworth <andy@accreu.com>
2019-12-14 20:58:05 -0600

    Align news icon right

--- client/src/components/MainContent.js
+++ client/src/components/MainContent.js
@@ -3,20 +3,43 @@
  */
 
 import React from 'react';
+import { Row, Column } from '@wurde/components';
 import { Button, Icon } from '@wurde/components';
 import ArticleList from './ArticleList';
+
+/**
+ * Define styles
+ */
+
+const IconStyle = {
+  float: 'right',
+  cursor: 'pointer',
+  marginRight: '10px', 
+}
 
 /**
  * Define component
  */
 
 function MainContent() {
+  function toggleFeedList() {
+    alert('feed');
+  }
+
   return (
     <main>
-      <hr />
-      <Icon type="newspaper-o" fill="#348FF0" />
+      <Row>
+        <Column>
+          <hr />
+          <Icon type="newspaper-o" onClick={toggleFeedList} style={IconStyle} />
+        </Column>
+      </Row>
 
-      <ArticleList />
+      <Row>
+        <Column>
+          <ArticleList />
+        </Column>
+      </Row>
     </main>
   );
 }
